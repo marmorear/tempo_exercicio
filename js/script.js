b41992a4a3d0c692ce2515a3f71e17cf149e6ac2
marmorear <clarasalodes@gmail.com>
2021-03-01 00:05:58 -0300

    Imagens adicionadas mas javascript atualizado e funcionando

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-alert('oi')
+function carregar() {
+  var msg = window.document.getElementById("msg");
+  var img = window.document.getElementById("imagem");
+  var data = new Date();
+  var hora = data.getHours();
+  msg.innerHTML = `Agora são ${hora} horas.`;
+
+  if (hora <= 0 && hora < 12) {
+    msg2.innerHTML='Bom dia!'
+    img.src = "./img/manha.png";
+    document.body.style.background ='#947a5c'
+  } else if (hora >= 12 && hora <= 18) {
+    msg2.innerHTML='Boa tarde!'
+    img.src = "./img/tarde.png";
+    document.body.style.background ='#8b5563'
+  } else {
+    msg2.innerHTML='Boa noite!'
+    img.src = "./img/noite.png";
+    document.body.style.background ='#0c6f74'
+  }
+}
